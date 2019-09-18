6857f0a79f16195daae4c7e9f5bdfde6e10f670f
Fabiana Fornerino <fabianafornerino@gmail.com>
2019-09-18 22:03:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-// let katzDeliLine = [];
+let katzDeliLine = [];
 
-// function takeANumber(katzDeliLine, name){
-//   katzDeliLine.push(name);
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
   
-//   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
-// }
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
 
-// function nowServing(katzDeliLine){
-//   if (katzDeliLine.length === 0){
-//     return "There is nobody waiting to be served!"
-//   }
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!"
+  }
   
-//   return `Currently serving ${katzDeliLine.shift()}.`
-// }
+  return `Currently serving ${katzDeliLine.shift()}.`
+}
 
-// function currentLine(katzDeliLine){
-//   let stringLine = 'The line is currently:'
+function currentLine(katzDeliLine){
+  let stringLine = 'The line is currently:'
   
-//   if(katzDeliLine.length ===0){
-//     return "The line is currently empty."
-//   }
+  if(katzDeliLine.length ===0){
+    return "The line is currently empty."
+  }
   
-//   for (let i=0; i < katzDeliLine.length; i++){
-//     if (i === katzDeliLine.length-1){
-//     stringLine+= ` ${i+1}. ${katzDeliLine[i]}`
-//     } else {
-//     stringLine+= ` ${i+1}. ${katzDeliLine[i]},`
-//     }
-//   }
+  for (let i=0; i < katzDeliLine.length; i++){
+    if (i === katzDeliLine.length-1){
+    stringLine+= ` ${i+1}. ${katzDeliLine[i]}`
+    } else {
+    stringLine+= ` ${i+1}. ${katzDeliLine[i]},`
+    }
+  }
   
-//   return stringLine
-// }
+  return stringLine
+}
 
 
 
